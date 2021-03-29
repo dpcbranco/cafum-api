5c3b9fab4cf613dc9861de44ebdf76b7bb2d997d
blanco0001 <vitor.blancobarbosa@gmail.com>
2021-03-29 20:42:05 -0300

    Alteração da topologia, com relação a conexão da base, seguindo as ultimas regras de segurança, fornecidos pelo MongoDB-Cloud.

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 module.exports = (uri) => {
     const db = mongoose.connection
-    mongoose.connect(uri)
+    // cria conexão, utilizando parametros da ultima versão do MongoDB
+    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
         console.log("Connected to database!")
