fe65c5e0796f4fdb7aff7b0504878c24a3f4ad7a
dpcbranco <dani.hyuuga@hotmail.com>
2021-03-29 22:23:53 -0300

    Permite obter pilotos com ordenação

--- controllers/pilotsController.js
+++ controllers/pilotsController.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 const Pilot = require("../models/Pilot")
 
-const _getAllPilots = async () => {
+const _getAllPilots = async (orderBy, desc) => {
     const pilotSchema = mongoose.model('Pilot', Pilot, "pilots")
-    return await pilotSchema.find()
+    return await pilotSchema.find().sort(orderBy ? {[orderBy]: desc ? -1 : 1} : {})
 }
 
 module.exports = {
--- routes/pilotsRouter.js
+++ routes/pilotsRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pilotsController = require('../controllers/pilotsController')
 
 router.get("/", async (req, res) => {
-    const pilots = await pilotsController.getAllPilots()
+    const pilots = await pilotsController.getAllPilots(req.query.orderBy, req.query.desc === 'true')
     res.status(200).send(pilots)
 })
 
