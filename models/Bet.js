e2a22513f900a3f52a5363125f3c393521295c39
dpcbranco <dani.hyuuga@hotmail.com>
2021-04-08 18:38:25 -0300

    Cria referência de DB dos dados da aposta

--- models/Bet.js
+++ models/Bet.js
@@ -2,8 +2,8 @@ const Schema = require("mongoose").Schema;
 
 module.exports = new Schema(
     {
-        userId: { type: Schema.ObjectId, ref: "User" },
-        gpId: { type: Schema.ObjectId, ref: "Bet" },
+        userId: { type: Schema.Types.ObjectId, ref: "User" },
+        gpId: { type: Schema.Types.ObjectId, ref: "Gp" },
         qualifying: [{ pilotId: String, position: Number }],
         race: [{ pilotId: String, position: Number, bestLap: Boolean }],
         totalPoints: Number,
--- services/bets.services.js
+++ services/bets.services.js
@@ -1,9 +1,13 @@
 const Bet = require("../models/Bet");
+const User = require("../models/User");
+const Gp = require("../models/Gp");
 const mongoose = require("mongoose");
-const betModel = mongoose.model("Bet", Bet, "bets");
+const betModel = mongoose.model("Bet", Bet);
+const userModel = mongoose.model("User", User);
+const gpModel = mongoose.model("Gp", Gp);
 
 const _findBet = async (filter) => {
-    return await betModel.findOne(filter);
+    return await betModel.find(filter).populate("gpId").populate("userId");
 };
 
 const _createBet = async (bet) => {
