5de86c0005e071f0b9d7e665fa9715de2e29451d
dpcbranco <dani.hyuuga@hotmail.com>
2021-04-10 18:38:53 -0300

    Acrescenta validação de duplicatas

--- validators/bets.validator.js
+++ validators/bets.validator.js
@@ -4,39 +4,91 @@ const betService = require("../services/bets.services");
 const pilotService = require("../services/pilots.service");
 
 const _validateNewBet = async (req, res, next) => {
+    let pilotBets = req.body.pilotBets ? req.body.pilotBets : [];
+    const errors = [];
+
     // Validates mandatory fields sent in the request body
-    if (!req.body.gpId)
-        return res.status(400).send({ error: "Error: gpId not informed" });
-    if (!req.body.userId)
-        return res.status(400).send({ error: "Error: userId not informed" });
+    if (!req.body.gpId) errors.push({ message: "gpId not informed" });
+    if (!req.body.userId) errors.push({ message: "userId not informed" });
+
+    // Validates duplicated pilots
+    const duplicatedPilots = [];
+
+    pilotBets.forEach((pilot) => {
+        if (
+            pilotBets.filter(
+                (pilotBet) => pilot === pilotBet || pilot.id === pilotBet.id
+            ).length > 1
+        )
+            duplicatedPilots.push(pilot);
+    });
+
+    if (duplicatedPilots.length > 0)
+        errors.push({
+            message: `Pilots duplicated on request body`,
+            duplicated: duplicatedPilots,
+        });
+
+    // Validates duplicated qualifying bets
+    const duplicatedQuali = [];
+    pilotBets.forEach((pilot) => {
+        if (
+            pilotBets.filter(
+                (pilotBet) =>
+                    pilot.qualifyingPosition === pilotBet.qualifyingPosition
+            ).length > 1
+        )
+            duplicatedQuali.push(pilot);
+    });
+
+    if (duplicatedQuali.length > 0)
+        errors.push({
+            message: `Qualifying bet position duplicated`,
+            duplicated: duplicatedQuali,
+        });
+
+    //Validates duplicated race bets
+    const duplicatedRace = [];
+    pilotBets.forEach((pilot) => {
+        if (
+            pilotBets.filter(
+                (pilotBet) => pilot.racePosition === pilotBet.racePosition
+            ).length > 1
+        )
+            duplicatedRace.push(pilot);
+    });
+
+    if (duplicatedRace.length > 0)
+        errors.push({
+            message: `Race bet position duplicated`,
+            duplicated: duplicatedRace,
+        });
+
+    if (errors.length > 0) return res.status(400).send({ errors });
 
     // Validates existence of user reference in database
     const user = await userService.findById(req.body.userId);
-    if (!user) return res.status(404).send({ error: "User not found." });
+    if (!user) return res.status(404).send({ message: "User not found." });
 
     // Validates existence of GP reference in database
     const gp = await gpService.findById(req.body.gpId);
-    if (!gp) return res.status(404).send({ error: "GP not found." });
+    if (!gp) return res.status(404).send({ message: "GP not found." });
 
-    let pilotBets = [];
-
-    // Validates pilot list from qualifying bet
-    if (req.body.pilotBets) {
-        const pilotsNotFound = [];
-        const pilotPromises = await req.body.pilotBets.map(async (pilot) => {
-            const pilotDB = await pilotService.findPilotById(pilot.id);
-            if (!pilotDB) pilotsNotFound.add(pilot.id);
-            return { id: pilotDB._id, ...pilot };
+    // Validates existence of pilot in database
+    const pilotsNotFound = [];
+    const pilotPromises = await pilotBets.map(async (pilot) => {
+        const pilotDB = await pilotService.findPilotById(pilot.id);
+        if (!pilotDB) pilotsNotFound.push(pilot.id);
+        return { id: pilotDB._id, ...pilot };
+    });
+    if (pilotsNotFound.length > 0)
+        return res.status(404).send({
+            message: "Pilots not found in database",
+            pilotsNotFound,
         });
-        if (pilotsNotFound.length > 0)
-            return res.status(404).send({
-                error: "Pilots not found in database",
-                pilotsNotFound,
-            });
-        await Promise.all(pilotPromises).then((results) => {
-            pilotBets = results;
-        });
-    }
+    await Promise.all(pilotPromises).then((results) => {
+        pilotBets = results;
+    });
 
     const betFilter = {
         userId: user._id,
@@ -47,7 +99,7 @@ const _validateNewBet = async (req, res, next) => {
     if (bet)
         return res
             .status(409)
-            .send({ error: "Bet already created for this GP." });
+            .send({ message: "Bet already created for this GP." });
 
     req.body = {
         ...betFilter,
