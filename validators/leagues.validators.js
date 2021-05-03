8f8c4d503aafa0e1288759f430a01a9fe09d6cfb
Daniel P. C. Branco <dani.hyuuga@hotmail.com>
2021-05-03 11:55:59 -0300

    Cria validadores de liga e acesso de manager.

---
+++ validators/leagues.validators.js
@@ -0,0 +1,30 @@
+const leagueService = require('../services/leagues.service');
+
+const _validateExistingLeague = async (req, res, next) => {
+    // Validates existence of league
+    const league = await leagueService.findLeagueById(req.params.leagueId);
+    if (!league) return res.status(404).send({
+        message: 'League not found'
+    });
+    res.league = league;
+    next();
+};
+
+const _validateManager = async (req, res, next) => {
+    const managers = res.league.members.filter(
+        (member) => member.owner || member.manager
+    );
+
+    // Verifies if user is manager or owner of the league
+    if (managers.none((manager) => manager.id.toString() === res.user._id))
+        return res.status(401).send(
+            { message: 'User not authorized to perform this action' }
+        );
+    
+    next();
+};
+
+module.exports = {
+    validateExistingLeague: _validateExistingLeague,
+    validateManager: _validateManager
+};
