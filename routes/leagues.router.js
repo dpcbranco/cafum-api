47aa4f03ce904827fe0bfbd89fb5734afb500efc
Daniel P. C. Branco <dani.hyuuga@hotmail.com>
2021-05-03 12:00:57 -0300

    Adiciona validadores às rotas

--- routes/leagues.router.js
+++ routes/leagues.router.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
 
 const leagueController = require('../controllers/leagues.controller');
+const leagueValidator = require('../validators/leagues.validators');
 
 router.post('/new', leagueController.createLeague);
-router.get('/:id', leagueController.findLeague);
-router.patch('/:leagueId/user/add/:userId', leagueController.addUser);
+router.get(
+    '/:leagueId',
+    leagueValidator.validateExistingLeague,
+    leagueController.findLeague
+);
+router.patch(
+    '/:leagueId/user/add/:userId',
+    leagueValidator.validateExistingLeague,
+    leagueValidator.validateManager,
+    leagueController.addUser
+);
 
 module.exports = router;
