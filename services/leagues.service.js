d4e40b164432763dc6c75745a39438cfd702b308
Daniel P. C. Branco <dani.hyuuga@hotmail.com>
2021-05-03 11:09:55 -0300

    Cria endpoint para adicionar membro à liga.

--- services/leagues.service.js
+++ services/leagues.service.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const League = require('../models/League');
+const User = require('../models/User');
+mongoose.model('User', User);
 const leagueSchema = mongoose.model('League', League);
 
 const _findLeagueById = async (id) => {
@@ -14,8 +16,15 @@ const _createLeague = async (league) => {
     return await leagueSchema.create(league);
 };
 
+const _updateLeague = async (id, patch) => {
+    const league = await leagueSchema.findById(id);
+    Object.assign(league, patch);
+    return await league.save();
+};
+
 module.exports = {
     findLeagueById: _findLeagueById,
     findLeagueByName: _findLeagueByName,
-    createLeague: _createLeague
+    createLeague: _createLeague,
+    updateLeague: _updateLeague
 };
