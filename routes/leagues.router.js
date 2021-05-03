72041bd13f7b655e5e65f8818152b471c63e6be2
Daniel P. C. Branco <dani.hyuuga@hotmail.com>
2021-05-03 14:35:33 -0300

    Acrescenta validação na rota de criação de liga.

--- routes/leagues.router.js
+++ routes/leagues.router.js
@@ -3,7 +3,11 @@ const router = require('express').Router();
 const leagueController = require('../controllers/leagues.controller');
 const leagueValidator = require('../validators/leagues.validators');
 
-router.post('/new', leagueController.createLeague);
+router.post(
+    '/new',
+    leagueValidator.validateNewLeague,
+    leagueController.createLeague
+);
 router.get(
     '/:leagueId',
     leagueValidator.validateExistingLeague,
