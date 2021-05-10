8bee8accc82fe995367fa92b3d37a44b47622a75
Daniel P. C. Branco <dani.hyuuga@hotmail.com>
2021-05-10 20:21:56 -0300

    Reorganiza validações da nova aposta.

--- routes/bets.router.js
+++ routes/bets.router.js
@@ -1,16 +1,14 @@
 const router = require('express').Router();
 const betValidator = require('../validators/bets.validator');
 const gpValidator = require('../validators/gps.validator');
-const leagueValidator = require('../validators/leagues.validators');
 const betsController = require('../controllers/bets.controller');
 
 router.get('/user/:userId', betsController.getBetByUser);
 router.post(
     '/new',
     betValidator.validateNewBet,
-    betValidator.validateBetConflict,
     gpValidator.validateGpExistence,
-    leagueValidator.validateLeagueExistence,
+    betValidator.validateBetConflict,
     betValidator.validateBetPilots,
     betsController.postNewBet
 );
