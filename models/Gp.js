b2ee19e606b53847b27da35fac84bf6c272c289c
Daniel P. C. Branco <dani.hyuuga@hotmail.com>
2021-05-19 22:43:21 -0300

    Adiciona resultado ao modelo de GP

--- models/Gp.js
+++ models/Gp.js
@@ -2,4 +2,11 @@ const Schema = require('mongoose').Schema;
 
 module.exports = new Schema({
     id: Schema.Types.ObjectId,
-});
+    result: {
+        finishers: [{
+            pilot: { type: Schema.Types.ObjectId, ref: 'Pilot' },
+            bestLap: Schema.Types.Boolean
+        }],
+        dnf: [{ type: Schema.Types.ObjectId, ref: 'Pilot' }]
+    }
+},{ versionKey: false });
