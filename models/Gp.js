3ece14de4c6132d0c35c6730677784d1c8d3f36a
Daniel P. C. Branco <dani.hyuuga@hotmail.com>
2021-05-20 16:41:16 -0300

    Corrige resultado da corrida no modelo

--- models/Gp.js
+++ models/Gp.js
@@ -2,7 +2,7 @@ const Schema = require('mongoose').Schema;
 
 module.exports = new Schema({
     id: Schema.Types.ObjectId,
-    result: {
+    raceResult: {
         finishers: [{
             pilot: { type: Schema.Types.ObjectId, ref: 'Pilot' },
             bestLap: Schema.Types.Boolean
