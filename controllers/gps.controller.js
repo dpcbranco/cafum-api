809aafbbcd010997a4ba4918eaf75296a9e96e23
Daniel P. C. Branco <dani.hyuuga@hotmail.com>
2021-05-20 18:15:03 -0300

    Cria API para atualizar pontos da quali nas ligas

--- controllers/gps.controller.js
+++ controllers/gps.controller.js
@@ -37,4 +37,38 @@ const calculateRaceResults = async (req, res) => {
     );
 };
 
-module.exports = { calculateRaceResults };
+const calculateQualiResults = async (req, res) => {
+    const currentRace = await gpService.findCurrentRace();
+    if (!currentRace.qualiResult) 
+        return res.status(200).send({ message: 'Results not in yet.' });
+    
+    const gpBets = await betService.findBet({ gpId: currentRace._id });
+    const leagues = await leagueService.getAllLeagues();
+
+    gpBets.forEach(
+        async (bet) => {
+            const league = leagues.find(
+                (league) => bet.leagueId.toString() === league._id.toString()
+            );
+            const totalPoints = bet.totalPoints + betUtils.calculateQualiPoints(
+                bet, league.pointSystem, currentRace.qualiResult
+            );
+            const memberIndex = league.members.findIndex(
+                (member) => member.user.toString() === bet.userId.toString() 
+            );
+            league.members[memberIndex].points += totalPoints;
+
+            betService.updateBet(bet, { totalPoints });
+        }
+    );
+
+    leagues.forEach((league) => 
+        leagueService.updateLeague(league._id, { members: league.members })
+    );
+
+    return res.status(200).send(
+        { message: 'Points calculations successfully started' }
+    );
+};
+
+module.exports = { calculateRaceResults, calculateQualiResults };
