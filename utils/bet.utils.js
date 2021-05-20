9e88a97503a50f5c7fec4ca22fb1da965042587f
Daniel P. C. Branco <dani.hyuuga@hotmail.com>
2021-05-20 17:39:47 -0300

    Corrige nome da função de cálcuo de pontos

--- utils/bet.utils.js
+++ utils/bet.utils.js
@@ -1,31 +1,30 @@
-const calculateRacePoints = (bet, pointSystem, result) => {
+const calculateRacePoints = (bet, pointSystem, raceResult) => {
     let finalPoints = 0;
 
-    if (bet.pilotBets.length > 0){
-        bet.pilotBets.forEach((pilot) => {
-            const finishers = result.finishers;
-            const pilotResult = finishers.findIndex(
-                pr => pr.pilot.toString() === pilot.id.toString()
-            ) + 1;
+    bet.pilotBets.forEach((pilot) => {
+        const finishers = raceResult.finishers;
+        const pilotResult = finishers.findIndex(
+            pr => pr.pilot.toString() === pilot.id.toString()
+        ) + 1;
 
-            if (pilotResult) {
-                // Difference between bet and actual result
-                const betResultDiff = pilotResult - pilot.racePosition;
+        if (pilotResult) {
+            // Difference between bet and actual result
+            const betResultDiff = pilotResult - pilot.racePosition;
                 
-                if (betResultDiff === 0)
-                    finalPoints += pointSystem.raceCorrect;
-                else if (Math.abs(betResultDiff) === 1)
-                    finalPoints += pointSystem.raceCloseCall;
+            if (betResultDiff === 0)
+                finalPoints += pointSystem.raceCorrect;
+            else if (Math.abs(betResultDiff) === 1)
+                finalPoints += pointSystem.raceCloseCall;
 
-                if (finishers[pilotResult - 1].bestLap && pilot.bestLap)
-                    finalPoints += pointSystem.fastestLap;
-            } 
-        });
-    }
+            if (finishers[pilotResult - 1].bestLap && pilot.bestLap)
+                finalPoints += pointSystem.fastestLap;
+        } 
+    });
+
     return finalPoints;
 
 };
 
 module.exports = {
-    calculateBetPoints: calculateRacePoints
+    calculateRacePoints
 };
