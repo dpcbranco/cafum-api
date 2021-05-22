a0e978f9308606ca9bb023d62486d41f3ed94847
Daniel P. C. Branco <dani.hyuuga@hotmail.com>
2021-05-22 19:51:00 -0300

    Cria conversor de resultado de corrida

--- utils/ergast.utils.js
+++ utils/ergast.utils.js
@@ -3,7 +3,7 @@ const pilotService = require('../services/pilots.service');
 const qualiToDBModel = async (qualiResult) => {  
     const pilotPromises = await qualiResult.map(async (driver) => {
         const pilot = await pilotService.findPilotByNumber(+driver.number);
-        return pilot ? pilot._id : undefined;
+        return  pilot._id;
     });
     let quali = [];
     await Promise.all(pilotPromises).then((results) => {
@@ -12,4 +12,26 @@ const qualiToDBModel = async (qualiResult) => {
     return quali;
 };
 
-module.exports = { qualiToDBModel };
+const raceToDBModel = async (raceResult) => {
+    const finishers = [];
+    const dnf = [];
+
+    const pilotPromises = await raceResult.map(async (driver) => {
+        const pilot = await pilotService.findPilotByNumber(+driver.number);
+        const raceStatus = driver.status;
+        if (raceStatus === 'Finished' || /\+\d (Lap|Laps)/.test(raceStatus)) 
+            finishers.push({
+                pilot: pilot._id,
+                bestLap: driver.FastestLap.rank === '1'
+            });
+        else dnf.push(pilot._id);
+    });
+
+    for (let pilotPromise of pilotPromises){
+        await Promise.resolve(pilotPromise);
+    }
+    
+    return { finishers, dnf };
+};
+
+module.exports = { qualiToDBModel, raceToDBModel };
